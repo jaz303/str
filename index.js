a8182070330518bdde4f84737caa1d555be05286
Jason Frame <jason@onehackoranother.com>
2015-09-11 23:50:15 +0100

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+var parser = require('./lib/parser');
+var evaluate = require('./lib/evaluate');
+
+module.exports = function() {
+	return {
+		compile: function(source) {
+			return parser.parse(source);
+		},
+		evaluate: evaluate,
+		createEnvironment: require('./lib/env').create
+	}
+};
